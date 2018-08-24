6ce699ec98708ea3444cc1d8a35bdc39774facd7
Michael M <12465142+Mike15678@users.noreply.github.com>
2018-08-24 17:23:56 -0400

    OK

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 source /etc/profile.d/devkit-env.sh
-export PATH=$PATH:/bannertool/output/linux-x86_64/
+export PATH=$PATH:/bannertool/output/linux-x86_64/:/3dstool/
 make -C /seedminer/seedstarter/
