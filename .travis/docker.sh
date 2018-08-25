40251d1230ca6d58bbc4f3d783f9a9e6ec8eef95
Michael M <12465142+Mike15678@users.noreply.github.com>
2018-08-24 21:27:24 -0400

    This """"might""" work

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -2,7 +2,7 @@
 
 source /etc/profile.d/devkit-env.sh
 cp /bannertool/output/linux-x86_64/bannertool /usr/bin/
-cp /3dstool/3dstool /usr/bin/
+cp /3dstool/. /usr/bin/
 cp /makerom/makerom /usr/bin/
 cd /seedminer/seedstarter/
 make
