49c6cf420e086780da634c541e210c976d16a606
Michael M <12465142+Mike15678@users.noreply.github.com>
2018-08-24 21:47:54 -0400

    Update docker.sh

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -4,5 +4,7 @@ source /etc/profile.d/devkit-env.sh
 cp /bannertool/output/linux-x86_64/bannertool /usr/bin/
 cp -r /3dstool/. /usr/bin/
 cp /makerom/makerom /usr/bin/
+cd /seedminer/
+ls -l
 cd /seedminer/seedstarter/
 make
