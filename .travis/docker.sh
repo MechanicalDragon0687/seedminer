68df50160b2b5046eeaa9667ac4938a5fff687dc
Michael M <12465142+Mike15678@users.noreply.github.com>
2018-08-24 20:36:02 -0400

    oof

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
 source /etc/profile.d/devkit-env.sh
-cp /bannertool/output/linux-x86_64/bannertool /seedminer/seedstarter/
-cp /3dstool/3dstool /seedminer/seedstarter/
-cp /makerom/makerom /seedminer/seedstarter/
+cp /bannertool/output/linux-x86_64/bannertool /usr/bin/
+cp /3dstool/build/3dstool /usr/bin/
+cp /makerom/makerom /usr/bin/
 cd /seedminer/seedstarter/
 make
 ls
