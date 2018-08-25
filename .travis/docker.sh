5078afc36056dc1b717e324fc9ba19c3c310bd69
Michael M <12465142+Mike15678@users.noreply.github.com>
2018-08-24 21:31:28 -0400

    a ha ha

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -2,8 +2,7 @@
 
 source /etc/profile.d/devkit-env.sh
 cp /bannertool/output/linux-x86_64/bannertool /usr/bin/
-cp /3dstool/. /usr/bin/
+cp -r /3dstool/. /usr/bin/
 cp /makerom/makerom /usr/bin/
 cd /seedminer/seedstarter/
 make
-ls
