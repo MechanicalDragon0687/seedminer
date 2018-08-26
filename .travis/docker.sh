a04db40e2ab81c2daff88b229634095c64291a40
Michael M <12465142+Mike15678@users.noreply.github.com>
2018-08-25 21:37:06 -0400

    Almost done [ci skip]

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -1,11 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 source /etc/profile.d/devkit-env.sh
 cp /bannertool/output/linux-x86_64/bannertool /usr/bin/
-cp -r /3dstool/. /usr/bin/
 cp /makerom/makerom /usr/bin/
-cd /seedminer/
-ls -l
-cd /seedminer/seedstarter/
-make
-ls -l
+make -C /seedminer/seedstarter/
